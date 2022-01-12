5343e3217dae03514513d26b25668591d3546e68
vitalii-hrynko <97296378+vitalii-hrynko@users.noreply.github.com>
2022-01-12 17:27:49 +0200

    Update high-load-average-email-notification.sh

--- 2-high-load-average-email-notification/high-load-average-email-notification.sh
+++ 2-high-load-average-email-notification/high-load-average-email-notification.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 CORES=`nproc`
-LA=`cat /proc/loadavg | cut -d" " -f 1 | cut -d"." -f 1`
+LIM=`echo - | awk -v i="$CORES" '{print i * 8 / 10}'` # Limit 80% CPU cores total usage
 while true
 do
-  if (( 0,8*$LA >= $CORES  )); then
+  CUR_LA=`cat /proc/loadavg | cut -d" " -f 1` # current LA for 5 minutes
+  LIM_LA=`echo - | awk -v i="$CUR_LA" '{print i * 8 / 10}'` # 80% from current LA value
+  if awk "BEGIN {exit !($LIM_LA >= $LIM)}"; then
    echo "Load average is high (`cat /proc/loadavg`) on $HOSTNAME" | mail -s "HIGH LOAD AVERAGE" root
-  else
-   echo "It's ok"
   fi
 sleep 300
 done
